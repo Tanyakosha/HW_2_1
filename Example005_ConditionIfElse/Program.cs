0bb9476c4e9a81c036d5a79edb795ff8b24bfeff
Tatyana <tanya.dyatchkova@gmail.com>
2022-11-29 12:26:58 +0300

    add Example006 Base

---
+++ Example005_ConditionIfElse/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower == "Таня");
+{
+    Console.WriteLine("Ура, это же ТАНЯ!");
+}
+else
+{
+    Console/TextWriter("Привет, ");
+    Console.WriteLine(username);
+}
+
